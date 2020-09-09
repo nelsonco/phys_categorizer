8a34c96aad90caa907bf3b9c0405f43169dc4e3b
Courtney Nelson <courtneya.nelson1@gmail.com>
2020-09-09 15:03:40 -0700

    update

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Mon Sep  7 22:37:00 2020
-
-@author: Court
-"""
-
 import proc_func
-import os 
 import pandas as pd
 
 import nltk
 from nltk.stem import WordNetLemmatizer
-from nltk import word_tokenize          
-from nltk.stem import WordNetLemmatizer 
+
 import nltk.tokenize as tok
 nltk.download('punkt')
 nltk.download('wordnet')
@@ -25,10 +16,13 @@ from sklearn.model_selection import GridSearchCV
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import make_pipeline
 from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegressionCV
+from sklearn.naive_bayes import GaussianNB
 
-import mglearn
-import matplotlib as plt
 
+import numpy as np
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
 
 main_directory = r'C:\Users\Court\Downloads\physics'
 
@@ -81,6 +75,7 @@ reg = tok.RegexpTokenizer(r'\w+') #Tokenizes without punctuation
 lemmatizer = WordNetLemmatizer()  #
 
 
+english_words = set(nltk.corpus.words.words())
 stop_words = text.ENGLISH_STOP_WORDS.union(['phys', 'rev', 'pop', 'mi', 'al'])
 
 
@@ -113,44 +108,15 @@ txt_train, txt_test, cat_train, cat_test = train_test_split(txts, categories, st
 # n-grams stored are those less that or equal to three. Additionally stop words which has been
 # defined above are removed. The count vectorizer stores the text as vectors which represent the 
 # number of occurances of a given token within a document.
-count_vect = CountVectorizer(tokenizer = LemmaTokenizer, min_df=3, ngram_range = (1,3), stop_words = stop_words)
+count_vect = CountVectorizer(tokenizer = LemmaTokenizer, min_df=30, ngram_range = (1,3), stop_words = stop_words)
 
 # Transform the txt from the training set and test set into the matrix representation of the frequency of their tokens
 Txt_train = count_vect.fit_transform(txt_train) # Also preform a fit on the txt_train which creates a dictionary of all the tokens in the documents
 Txt_test = count_vect.transform(txt_test)
 
 
-feature_names = count_vect.get_feature_names()
-
-print(feature_names[:10])
-
-parameters  = {"logisticregression__C": [0.01, 0.1, 1, 10, 100],
-              "tfidfvectorizer__ngram_range": [(1, 1),(1, 2), (1, 3)]} #, (1, 2), (1, 3)
-
-# The pipeline allows for one function to be preformed before another
-# The TF-IDF vectorizer is applied then the LogisticRegression algorithm is applied
-pipeline = make_pipeline(TfidfVectorizer(min_df=5), LogisticRegression())
-
-print('done')
-grid_search = GridSearchCV(pipeline, parameters)
-print('done')
-grid_search.fit(txt_train, cat_train)
-
-import plotly.express as px
-
-# extract scores from grid_search
-scores = grid_search.cv_results_['mean_test_score'].reshape(-1, 3)
 
 
-# visualize heat map
-fig = px.imshow(scores, labels = dict(x = 'N-Gram Value', y = 'C Value', color = 'Accuracy(%)'),
-                x = ['(1, 1)','(1, 2)', '(1, 3)'], y = ['C=100','C=10'])
-fig.show()
-
-from sklearn.linear_model import LogisticRegression
-from sklearn.linear_model import LogisticRegressionCV
-from sklearn.naive_bayes import GaussianNB
-from sklearn.feature_extraction.text import TfidfTransformer
 
 # When setting a parameter to loop through the form is: function name + __ + parameter
 n_parameters  = {'countvectorizer__ngram_range': [(1, 1),(1, 2), (1, 3)]}
@@ -159,41 +125,174 @@ n_parameters  = {'countvectorizer__ngram_range': [(1, 1),(1, 2), (1, 3)]}
 # The pipeline allows for one function to be preformed before another and the GridSearch 
 # iterates through the parameters
 
-pipeline_lr = make_pipeline(CountVectorizer(min_df=100), LogisticRegressionCV())
-grid_search_lr = GridSearchCV(pipeline_lr, n_parameters)
-grid_search_lr.fit(txt_train, cat_train)
+count_vect_1 = CountVectorizer(tokenizer = LemmaTokenizer, min_df=30, ngram_range = (1,1), stop_words = stop_words)
+Txt_train_1 = count_vect_1.fit_transform(txt_train) # Also preform a fit on the txt_train which creates a dictionary of all the tokens in the documents
+Txt_test_1 = count_vect_1.transform(txt_test)
 
-pipeline_gnb = make_pipeline(CountVectorizer(min_df=100), GaussianNB())
-grid_search_gnb = GridSearchCV(pipeline_gnb, n_parameters)
-grid_search_gnb.fit(txt_train, cat_train)
+count_vect_2 = CountVectorizer(tokenizer = LemmaTokenizer, min_df=30, ngram_range = (1,2), stop_words = stop_words)
+Txt_train_2 = count_vect_2.fit_transform(txt_train) # Also preform a fit on the txt_train which creates a dictionary of all the tokens in the documents
+Txt_test_2 = count_vect_2.transform(txt_test)
+
+count_vect_3 = CountVectorizer(tokenizer = LemmaTokenizer, min_df=30, ngram_range = (1,3), stop_words = stop_words)
+Txt_train_3 = count_vect_3.fit_transform(txt_train) # Also preform a fit on the txt_train which creates a dictionary of all the tokens in the documents
+Txt_test_3 = count_vect_3.transform(txt_test)
+
+gnb_1 = GaussianNB()
+gnb_1.fit(Txt_train_1.toarray(), cat_train)
+gnb_2 = GaussianNB()
+gnb_2.fit(Txt_train_2.toarray(), cat_train)
+gnb_3 = GaussianNB()
+gnb_3.fit(Txt_train_3.toarray(), cat_train)
+
+lr_1 = LogisticRegressionCV()
+lr_1.fit(Txt_train_1.toarray(), cat_train)
+lr_2 = LogisticRegressionCV()
+lr_2.fit(Txt_train_2.toarray(), cat_train)
+lr_3 = LogisticRegressionCV()
+lr_3.fit(Txt_train_3.toarray(), cat_train)
 
 
 # The TF-IDF vectorizer is applied then the LogisticRegression algorithm is applied
-pipeline_lr_tfidf = make_pipeline(TfidfVectorizer(min_df=100), LogisticRegressionCV())
-grid_search_lr_tfidf = GridSearchCV(pipeline, parameters)
-grid_search_lr_tfidf.fit(txt_train, cat_train)
 
-import matplotlib.pyplot as plt
-import numpy as np
-import plotly.graph_objects as go
+count_vect_1_tf = TfidfVectorizer(tokenizer = LemmaTokenizer, min_df=30, ngram_range = (1,1), stop_words = stop_words)
+Txt_train_1_tf = count_vect_1_tf.fit_transform(txt_train) # Also preform a fit on the txt_train which creates a dictionary of all the tokens in the documents
+Txt_test_1_tf = count_vect_1_tf.transform(txt_test)
+
+count_vect_2_tf = TfidfVectorizer(tokenizer = LemmaTokenizer, min_df=30, ngram_range = (1,2), stop_words = stop_words)
+Txt_train_2_tf = count_vect_2_tf.fit_transform(txt_train) # Also preform a fit on the txt_train which creates a dictionary of all the tokens in the documents
+Txt_test_2_tf = count_vect_2_tf.transform(txt_test)
+
+count_vect_3_tf = TfidfVectorizer(tokenizer = LemmaTokenizer, min_df=30, ngram_range = (1,3), stop_words = stop_words)
+Txt_train_3_tf = count_vect_3_tf.fit_transform(txt_train) # Also preform a fit on the txt_train which creates a dictionary of all the tokens in the documents
+Txt_test_3_tf = count_vect_3_tf.transform(txt_test)
+
+gnb_1_tf = GaussianNB()
+gnb_1_tf.fit(Txt_train_1_tf.toarray(), cat_train)
+gnb_2_tf = GaussianNB()
+gnb_2_tf.fit(Txt_train_2_tf.toarray(), cat_train)
+gnb_3_tf = GaussianNB()
+gnb_3_tf.fit(Txt_train_3_tf.toarray(), cat_train)
+
+lr_1_tf = LogisticRegressionCV()
+lr_1_tf.fit(Txt_train_1_tf.toarray(), cat_train)
+lr_2_tf = LogisticRegressionCV()
+lr_2_tf.fit(Txt_train_2_tf.toarray(), cat_train)
+lr_3_tf = LogisticRegressionCV()
+lr_3_tf.fit(Txt_train_3_tf.toarray(), cat_train)
+
+#Calculate the accuracy of each method
+gnb_1_s = gnb_1.score(Txt_test_1.toarray(), cat_test)
+gnb_2_s = gnb_2.score(Txt_test_2.toarray(), cat_test)
+gnb_3_s = gnb_3.score(Txt_test_3.toarray(), cat_test)
+gnb_1_tf_s = gnb_1_tf.score(Txt_test_1_tf.toarray(), cat_test)
+gnb_2_tf_s = gnb_2_tf.score(Txt_test_2_tf.toarray(), cat_test)
+gnb_3_tf_s = gnb_3_tf.score(Txt_test_3_tf.toarray(), cat_test)
+lr_1_s = lr_1.score(Txt_test_1.toarray(), cat_test)
+lr_2_s = lr_2.score(Txt_test_2.toarray(), cat_test)
+lr_3_s = lr_3.score(Txt_test_3.toarray(), cat_test)
+lr_1_tf_s = lr_1_tf.score(Txt_test_1_tf.toarray(), cat_test)
+lr_2_tf_s = lr_2_tf.score(Txt_test_2_tf.toarray(), cat_test)
+lr_3_tf_s = lr_3_tf.score(Txt_test_3_tf.toarray(), cat_test)
 
 
-gnb_p_3 = [gnb_score, gnb_score]
-lr_p_3 = [lr_score, lr_score]
-tfidf_p_3 = [tfidf_lr_score, tfidf_lr_score]
+gnb_scores = [gnb_1_s, gnb_2_s, gnb_3_s]
+lr_scores = [lr_1_s, lr_2_s, lr_3_s]
+lr_tf_scores = [lr_1_tf_s, lr_2_tf_s, lr_3_tf_s]
+gnb_tf_scores = [gnb_1_tf_s, gnb_2_tf_s, gnb_3_tf_s]
+print('Accuracy as a function of n-gram range')
+print('Gaussian Naive Bayes Accuracy:', gnb_scores)
+print('Gaussian Naive Bayes TF-IDF Weighted Accuracy:',gnb_tf_scores)
+print('Logistic Regression Accuracy:', lr_scores)
+print('Logistic Regression TF-IDF Weighted Accuracy:',lr_tf_scores)
 
-colors = ['lavenderblush', 'lawngreen', 'lightblue']
 
-algorithm_typ = ['Naive', 'Logistic', 'TF-IDF Logistic',]
-n = ['N=1', 'N=2']
+algorithm_typ = ['Gaussian Naive Bayes','Gaussian TF-IDF Naive Bayes' 'Logistic', 'TF-IDF Logistic',]
+n = ['N=1', 'N=2','N=3']
 
 fig = go.Figure()
-fig.add_trace(go.Bar(x = n, y = gnb_p, name = 'Gausian Naive Bayes', marker_color = 'blue', width = 0.1, text = gnb_p))
-fig.add_trace(go.Bar(x = n, y = lr_p, name='Logistic Regression', marker_color = 'green', width = 0.1, text = lr_p))
-fig.add_trace(go.Bar(x = n, y = tfidf_p, name='TF-IDF Logistic Regression', marker_color = 'purple', width = 0.1, text = tfidf_p))
-fig.update_traces(texttemplate='%{text:.3f}', textposition='outside')
-fig.update_layout(title='Accuracy of Machine Learning Algorithms vs N-Gram Value',
-    xaxis_tickfont_size=14, yaxis=dict(title='Acuracy (%)', titlefont_size=16, tickfont_size=14,),
-    legend = dict(x = 1, y = 1), barmode='group', bargap=.6, bargroupgap=.3)
+fig.add_trace(go.Bar(x = n, y = gnb_scores, name = 'Gaussian Naive Bayes', marker_color = 'lightblue', width = 0.2, text = gnb_scores))
+fig.add_trace(go.Bar(x = n, y = gnb_tf_scores, name = 'TF-IDF Gaussian Naive Bayes', marker_color = 'blue', width = 0.2, text = gnb_tf_scores))
 
-fig.show()
+fig.add_trace(go.Bar(x = n, y = lr_scores, name = 'Logistic Regression', marker_color = 'plum', width = 0.2, text = lr_scores))
+fig.add_trace(go.Bar(x = n, y = lr_tf_scores, name = 'TF-IDF Logistic Regression', marker_color = 'purple', width = 0.2, text = lr_tf_scores))
+fig.update_traces(texttemplate = '%{text:.3f}', textposition = 'outside', textfont_size = 12)
+fig.update_layout(title='Accuracy of Machine Learning Algorithms vs N-Gram Value',
+                  uniformtext_minsize = 12,
+    xaxis_tickfont_size = 12, yaxis = dict(title = 'Accuracy (%)', titlefont_size = 13, tickfont_size = 12),
+    legend = dict(x = 1, y = 1), barmode = 'group', bargap = .1, bargroupgap = .3)
+
+fig.write_image('fig1.png',scale=5)
+
+# Look at the effect of the C value on the Logistic Regression Accuracy
+parameters  = {"logisticregression__C": [0.01, 0.1, 1, 10, 100],
+              "tfidfvectorizer__ngram_range": [(1, 1),(1, 2), (1, 3)]} #, (1, 2), (1, 3)
+
+# The pipeline allows for one function to be preformed before another
+# The TF-IDF vectorizer is applied then the LogisticRegression algorithm is applied
+pipeline = make_pipeline(TfidfVectorizer(tokenizer = LemmaTokenizer, min_df=30, stop_words = stop_words), LogisticRegression())
+
+grid_search = GridSearchCV(pipeline, parameters)
+grid_search.fit(txt_train, cat_train)
+
+# extract scores from grid_search
+scores = grid_search.cv_results_['mean_test_score'].reshape(-1, 3).T
+print(scores)
+ordered_scores = [scores[0],scores[2],scores[1]]
+print(ordered_scores)
+s_text = np.around(ordered_scores, decimals=4) # Only show rounded value (full value on hover)
+
+
+# visualize heat map
+fig0 = ff.create_annotated_heatmap(s_text, showscale=True,
+               y = ['n-grams = (1, 1)','n-grams = (1, 2)', 'n-grams = (1, 3)'], 
+                                  x = ['C = 0.01','C = 0.1','C = 1','C = 10','C = 100'])
+
+fig0.write_image('heatmap.png',scale=5)
+
+
+# Return what the sklearn toolkit found as the best value of the C parameter for the (1,3)
+# n-gram model
+print('Most effective C value: ',  lr_3_tf.C_[0])
+c_eff = lr_3_tf.C_[0]
+lr_3_tf_eff = LogisticRegression(C=c_eff)
+lr_3_tf_eff.fit(Txt_train_3_tf.toarray(), cat_train)
+print(lr_3_tf_eff.score(Txt_test_3_tf.toarray(), cat_test))
+
+feature_names = count_vect_3_tf.get_feature_names()
+term_imp = lr_3_tf_eff.coef_.ravel() #Gives the term importance after ifidf weighting
+tech_imp = np.argsort(term_imp)[-20:]
+tech_imp = [int(x) for x in tech_imp]
+feature_names_tech = [feature_names[x] for x in tech_imp]
+nontech_imp = np.argsort(term_imp)[:20]
+feature_names_nontech = [feature_names[x] for x in nontech_imp]
+
+tot_imp = np.concatenate([nontech_imp,tech_imp])
+tot_features = np.concatenate([feature_names_nontech,feature_names_tech])
+
+
+fig2 = go.Figure()
+
+fig2.add_trace(go.Bar(x = tot_features,
+                     y = term_imp[tot_imp], 
+                      width = 0.4, text = gnb_scores))
+fig2.update_layout(title='Coefficient Value for 40 Most Influential Terms',
+                      xaxis_tickfont_size = 10,uniformtext_mode='show')
+fig2.write_image('fig3.png',scale=5)
+
+def LemmaTokenizer_altered(text):
+    tokens = reg.tokenize(text) # tokenize the text
+    lemmas = list()
+    for token in tokens:
+        if token.isalpha() and token not in stop_words and len(token)>1:
+            lemma = lemmatizer.lemmatize(token)
+            lemmas.append(lemma)
+        if token == 'References':
+            break
+    return lemmas
+count_vect_3_tf_alt = TfidfVectorizer(tokenizer = LemmaTokenizer_altered, min_df=30, ngram_range = (1,3), stop_words = stop_words)
+Txt_train_3_tf_alt = count_vect_3_tf_alt.fit_transform(txt_train) # Also preform a fit on the txt_train which creates a dictionary of all the tokens in the documents
+Txt_test_3_tf_alt = count_vect_3_tf_alt.transform(txt_test)
+lr_3_tf_alt_eff = LogisticRegression(C=c_eff)
+lr_3_tf_alt_eff.fit(Txt_train_3_tf_alt.toarray(), cat_train)
+print('After removing tokens with length one the accuracy is:',
+      lr_3_tf_alt_eff.score(Txt_test_3_tf_alt.toarray(), cat_test))
